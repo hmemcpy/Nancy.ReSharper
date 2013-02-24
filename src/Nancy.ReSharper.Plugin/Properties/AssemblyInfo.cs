33923b74d7788cb8626e4f07b567f8cd8189ded4
hmemcpy <hmemcpy@gmail.com>
2013-02-24 15:30:58 +0200

    Adding update notifications

--- src/Nancy.ReSharper.Plugin/Properties/AssemblyInfo.cs
+++ src/Nancy.ReSharper.Plugin/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using JetBrains.Application.PluginSupport;
 [assembly: PluginDescription("Adds support for NancyFX in ReSharper")]
 [assembly: PluginVendor("Igal Tabachnik")]
 
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
