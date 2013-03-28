5533a090da2c6ca0a5daba06fb867826ed1f3c00
Igal Tabachnik <hmemcpy@gmail.com>
2013-03-28 22:42:38 +0200

    Preventing ASPX controls generation on Alt-Ins. This fixes #8

---
+++ src/Nancy.ReSharper.Plugin/LiveTemplates/NancyAspCSharpProjectScopeProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using JetBrains.Application;
+using JetBrains.ReSharper.Feature.Services.Asp.CSharp.LiveTemplates;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Context;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Scope;
+
+namespace Nancy.ReSharper.Plugin.LiveTemplates
+{
+    [ShellComponent]
+    public class NancyAspCSharpProjectScopeProvider : AspCSharpProjectScopeProvider
+    {
+        public override IEnumerable<ITemplateScopePoint> ProvideScopePoints(TemplateAcceptanceContext context)
+        {
+            yield break;
+        }
+    }
+}
