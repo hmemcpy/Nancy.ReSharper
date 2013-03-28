bd64f78b549fab990f73d3e0cf16e649e64b46ad
Igal Tabachnik <hmemcpy@gmail.com>
2013-03-28 02:31:25 +0200

    Managed to add a hack to prevent ASPX from appearing. Extremely flaky.
    Will try to find a better way.

---
+++ src/Nancy.ReSharper.Plugin/QuickFixes/NancyQuickFixTemplateProviderBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Intentions.Web.QuickFixes.Mvc;
+using Nancy.ReSharper.Plugin.CustomReferences;
+
+namespace Nancy.ReSharper.Plugin.QuickFixes
+{
+    public abstract class NancyQuickFixTemplateProviderBase : IMvcQuickFixTemplateProvider
+    {
+        public abstract Guid GetTemplateGuid(bool viewPage, bool withMasterpage);
+
+        public abstract string GetExtension(bool viewPage, bool withMasterpage);
+
+        public string GetQuickFixTitle(bool viewPage, bool withMasterpage)
+        {
+            if (!viewPage)
+                return "Create Razor partial view '{0}'";
+            return !withMasterpage ? "Create Razor view '{0}'" : "Create Razor view '{0}' with layout";
+        }
+
+        public bool IsAvailable(IProjectItem context)
+        {
+            return NancyCustomReferencesSettings.IsProjectReferencingNancyRazorViewEngine(context);
+        }
+    }
+}
