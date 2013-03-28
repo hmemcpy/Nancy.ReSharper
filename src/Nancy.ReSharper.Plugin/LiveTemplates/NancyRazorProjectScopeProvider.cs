01818de0fcd93d650b5ea1efb4d0653fca5d9093
Igal Tabachnik <hmemcpy@gmail.com>
2013-03-28 22:34:55 +0200

    Adding back generating Razor templates on Alt-Ins

---
+++ src/Nancy.ReSharper.Plugin/LiveTemplates/NancyRazorProjectScopeProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Application;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Context;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Scope;
+using JetBrains.ReSharper.Feature.Services.Razor.LiveTemplates.Scope;
+using JetBrains.Util;
+using Nancy.ReSharper.Plugin.CustomReferences;
+
+namespace Nancy.ReSharper.Plugin.LiveTemplates
+{
+    [ShellComponent]
+    public class NancyRazorProjectScopeProvider : ScopeProvider
+    {
+        public NancyRazorProjectScopeProvider()
+        {
+            Creators.AddRange(new List<Func<string, ITemplateScopePoint>>
+            {
+                TryToCreate<InAnyRazorProject>
+            });
+        }
+
+        public override IEnumerable<ITemplateScopePoint> ProvideScopePoints(TemplateAcceptanceContext context)
+        {
+            IProject project = context.GetProject();
+            if (project != null && NancyCustomReferencesSettings.IsProjectReferencingNancyRazorViewEngine(project))
+                yield return new InAnyRazorProject();
+        }
+    }
+}
