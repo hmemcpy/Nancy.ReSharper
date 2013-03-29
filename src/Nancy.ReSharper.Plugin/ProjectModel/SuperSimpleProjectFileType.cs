7f9a672a39f8b8cd925e294244eb9f1f0a0e502b
Igal Tabachnik <hmemcpy@gmail.com>
2013-03-29 15:50:09 +0300

    Adding initial SSVE support (starting with file type)

---
+++ src/Nancy.ReSharper.Plugin/ProjectModel/SuperSimpleProjectFileType.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+using JetBrains.ProjectModel;
+
+namespace Nancy.ReSharper.Plugin.ProjectModel
+{
+    [ProjectFileTypeDefinition(Name, Edition = "CSharp")]
+    public class SuperSimpleProjectFileType : HtmlProjectFileType
+    {
+        public new const string Name = "SSVE";
+        public new const string PresentableName = "Nancy Super Simple View";
+        
+        [CanBeNull]
+        public new static readonly SuperSimpleProjectFileType Instance;
+
+        private SuperSimpleProjectFileType()
+            : base(Name, PresentableName, new[] { ".sshtml" })
+        {
+        }
+
+        protected SuperSimpleProjectFileType(string name)
+            : base(name)
+        {
+        }
+
+        protected SuperSimpleProjectFileType(string name, string presentableName)
+            : base(name, presentableName)
+        {
+        }
+    }
+}
